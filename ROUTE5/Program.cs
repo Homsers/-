70947a8abf394829d1fae97d1713931da1515ace
Maxim <you@example.com>
2022-08-05 19:02:34 +0300

    expml5

---
+++ ROUTE5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == " Masha")
+{
+    Console.WriteLine("Ura, this is Masha!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
